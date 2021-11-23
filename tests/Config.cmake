1d3227c7a8837435fe37cc5d04df386beaf5b6a3
Alexander Saal <developer@olbaflinx.chm-projects.de>
2021-11-23 17:43:35 +0100

    - Update workflow - Removed libchipcard dependies (comes letter)

--- tests/Config.cmake
+++ tests/Config.cmake
@@ -1,18 +1,16 @@
-find_package(PkgConfig)
 find_package(KtoBlzCheck 1.53)
 find_package(gwenhywfar 5.7)
 find_package(gwengui-cpp 5.7)
 find_package(gwengui-qt5 5.7)
 find_package(aqbanking 6.3)
-pkg_check_modules(libchipcard-client REQUIRED IMPORTED_TARGET libchipcard-client>=${LIBCHIPCARD_MIN_VERSION})
 
 set(QT_LIBS ${Qt${QT_VERSION_MAJOR}Sql_LIBRARIES} ${Qt${QT_VERSION_MAJOR}Test_LIBRARIES} SingleApplication::SingleApplication)
 
 set(QT_INCLUDES ${Qt${QT_VERSION_MAJOR}Sql_INCLUDE_DIRS} ${Qt${QT_VERSION_MAJOR}Test_INCLUDE_DIRS})
 include_directories(${QT_INCLUDES})
 
-set(AQ_LIBS ${KtoBlzCheck_LIBRARIES} aqbanking::aqbanking gwenhywfar::core gwenhywfar::gui-cpp gwenhywfar::gui-qt5 ${libchipcard-client_LIBRARIES})
-set(AQ_INCLUDES ${AQBANKING_INCLUDE_DIRS} ${GWENHYWFAR_INCLUDE_DIRS} ${libchipcard-client_INCLUDE_DIRS})
+set(AQ_LIBS ${KtoBlzCheck_LIBRARIES} aqbanking::aqbanking gwenhywfar::core gwenhywfar::gui-cpp gwenhywfar::gui-qt5)
+set(AQ_INCLUDES ${AQBANKING_INCLUDE_DIRS} ${GWENHYWFAR_INCLUDE_DIRS})
 include_directories(${AQ_INCLUDES})
 
 add_definitions(${Qt${QT_VERSION_MAJOR}Sql_DEFINITIONS})
